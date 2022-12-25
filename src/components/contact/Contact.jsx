b7d5735f68b23fe0d78a7b59fa28d9d695416676
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-12-25 14:49:32 +0100

    deploying

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,15 +2,10 @@ import React from 'react'
 import './contact.css'
 import emailjs from 'emailjs-com'
 import { useRef } from 'react';
-import { useState } from 'react';
-import { initialState } from 'react';
-import { setState } from 'react';
 
 const Contact = () => {
-  const [statusMessage, setStatusMessage] = useState("");
 
-  
-  const clearState = () => setState({ ...initialState })
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -20,11 +15,9 @@ const Contact = () => {
     emailjs.sendForm('service_ng8n56j', 'template_w4xmtcn', form.current, 'jczRTBykows7XYT9O')
       .then((result) => {
           console.log(result.text, result.status);
-          clearState();
-          setStatusMessage("Email sent success");
+
       }, (error) => {
           console.log(error.text);
-          setStatusMessage(`${error.text} happened`);
       });
 
       e.target.reset()
@@ -38,7 +31,6 @@ const Contact = () => {
           <textarea name="message" rows="7" placeholder="Vänligen lämna information om namn på alla idioter som kommer, deras rank i Hearthstone, och deras allergier." required></textarea>
           <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
-        <p>{statusMessage}</p>
         </div>
     </section>
   )
